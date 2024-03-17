047d698209c902693ef1300dda7fffefd0dd58a7
Seyi <seyiobadee@gmail.com>
2024-03-16 20:13:57 -0500

    updated code

--- eks-kubeflow/network.tf
+++ eks-kubeflow/network.tf
@@ -1,49 +1,86 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = local.vpc_name
-  cidr = local.vpc_cidr
-
-  azs              = local.azs
-  private_subnets  = local.private_subnets
-  public_subnets   = local.public_subnets
-  database_subnets = local.db_subnets
-
-  create_database_subnet_group = true
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
+resource "aws_vpc" "main" {
+  cidr_block = local.vpc_cidr
+  enable_dns_support = true
   enable_dns_hostnames = true
-  enable_dns_support   = true
-  
-  public_subnet_tags = {
-    Name = "dev-eks-public-subnet"
-  }
-  private_subnet_tags = {
-    Name = "dev-eks-private-subnet"
-  }
-  database_subnet_tags = {
-    Name = "dev-eks-db-subnet"
-  }
-
   tags = {
-    Environment = "${var.environment}"
-    Name        = local.vpc_name
+    Name = "EKS-VPC"
   }
 }
 
+resource "aws_subnet" "public" {
+  count = length(local.public_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = local.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = element(local.azs, count.index)
+  tags = {
+    Name = "EKS-PublicSubnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(local.private_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = local.private_subnets[count.index]
+  availability_zone = element(local.azs, count.index)
+  tags = {
+    Name = "EKS-PrivateSubnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(local.db_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = localocal.db_subnets[count.index]
+  availability_zone = element(local.azs, count.index)
+  tags = {
+    Name = "EKS-DBSubnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "EKS-IGW"
+  }
+}
+
+resource "aws_eip" "nat1" {
+  domain = "vpc"
+}
+
+# resource "aws_eip" "nat2" {
+#   domain = "vpc"
+# }
+
+resource "aws_nat_gateway" "nat1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id = aws_subnet.public[0].id
+  tags = {
+    Name = "EKS-NAT1"
+  }
+}
+
+# resource "aws_nat_gateway" "nat2" {
+#   allocation_id = aws_eip.nat2.id
+#   subnet_id = aws_subnet.public[1].id
+#   tags = {
+#     Name = "EKS-NAT2"
+#   }
+# }
+
+#   create_database_subnet_group = true
+
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = module.vpc.vpc_id
+  vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.${local.region}.s3"
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id             = module.vpc.vpc_id
+  vpc_id             = aws_vpc.main.id
   service_name       = "com.amazonaws.${local.region}.ecr.api"
   vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
+  subnet_ids         = aws_subnet.private_subnets.id
   security_group_ids = [aws_security_group.ecr_sg.id]
 }
 
@@ -51,14 +88,14 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_id             = module.vpc.vpc_id
   service_name       = "com.amazonaws.${local.region}.ecr.dkr"
   vpc_endpoint_type  = "Interface"
-  subnet_ids         = module.vpc.private_subnets
+  subnet_ids         = aws_subnet.private_subnets.id
   security_group_ids = [aws_security_group.ecr_sg.id]
 }
 
 resource "aws_security_group" "ecr_sg" {
   name        = "ecr_sg"
   description = "Allow inbound traffic for ECR endpoint"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 443
