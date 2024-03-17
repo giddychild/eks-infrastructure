8f11f08d151e3ecaf112328acd824b52b12d088f
Seyi <seyiobadee@gmail.com>
2024-03-16 20:38:08 -0500

    updated code

--- eks-kubeflow/network.tf
+++ eks-kubeflow/network.tf
@@ -99,16 +99,16 @@ resource "aws_route_table" "private_az1" {
   }
 }
 
-resource "aws_route_table" "private_az2" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat2.id
-  }
-  tags = {
-    Name = "EKS-PrivateRouteTable-AZ2"
-  }
-}
+# resource "aws_route_table" "private_az2" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat2.id
+#   }
+#   tags = {
+#     Name = "EKS-PrivateRouteTable-AZ2"
+#   }
+# }
 
 resource "aws_route_table_association" "private_az1" {
   count          = length([for s in aws_subnet.private : s.id if s.availability_zone == "us-east-1a"])
@@ -116,11 +116,11 @@ resource "aws_route_table_association" "private_az1" {
   route_table_id = aws_route_table.private_az1.id
 }
 
-resource "aws_route_table_association" "private_az2" {
-  count          = length([for s in aws_subnet.private : s.id if s.availability_zone == "us-east-1b"])
-  subnet_id      = [for s in aws_subnet.private : s.id if s.availability_zone == "us-east-1b"][count.index]
-  route_table_id = aws_route_table.private_az2.id
-}
+# resource "aws_route_table_association" "private_az2" {
+#   count          = length([for s in aws_subnet.private : s.id if s.availability_zone == "us-east-1b"])
+#   subnet_id      = [for s in aws_subnet.private : s.id if s.availability_zone == "us-east-1b"][count.index]
+#   route_table_id = aws_route_table.private_az2.id
+# }
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
