0800983b6fe8bab50e2d55132bcc4bdd644e39af
Seyi <seyiobadee@gmail.com>
2024-03-16 19:49:48 -0500

    updated code

--- eks-kubeflow/locals.tf
+++ eks-kubeflow/locals.tf
@@ -4,10 +4,10 @@ locals {
   azs             = ["us-east-1a", "us-east-1b"]
   vpc_cidr        = "10.0.0.0/16"
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_names = ["${var.environment}-eks-private-subnet-a","${var.environment}-eks-private-subnet-b"]
+  private_subnet_names = ["dev-eks-private-subnet-a","dev-eks-private-subnet-b"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-  public_subnet_names = ["${var.environment}-eks-public-subnet-a","${var.environment}-eks-public-subnet-b"]
-  db_subnet_names = ["${var.environment}-eks-db-subnet-a","${var.environment}-eks-db-subnet-b"]
+  public_subnet_names = ["dev-eks-public-subnet-a","dev-eks-public-subnet-b"]
+  db_subnet_names = ["dev-eks-db-subnet-a","dev-eks-db-subnet-b"]
   db_subnets      = ["10.0.10.0/24", "10.0.11.0/24"]
   keycloak_user_password = var.KEYCLOAK_USER_PASSWORD
 }
